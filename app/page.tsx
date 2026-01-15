5c26f6280d16a05c468c3ab5bca52539b7a5e861
Hassan Sheikh <hassan.nadeem.4012@gmail.com>
2026-01-15 14:21:22 +0500

    Add Navbar folder in home also add 3 subfolder in Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import Home from "@/components/Home/Home";
+import React from "react";
 
 const HomePage = () => {
-  return (
-    <div className=''>
-      HomePage
-    </div>
-  )
-}
+  return <Home />;
+};
 
-export default HomePage
+export default HomePage;
+
+
+// localhost:3000
+// localhost:3000/about
+// localhost:3000/about/team
+// localhost:3000/about/team/member1
---
+++ components/Home/Navbar/Responsive.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Responsive = () => {
+  return (
+    <div>
+      <Nav />
+      <MobileNav />
+    </div>
+  )
+}
+
+export default Responsive
