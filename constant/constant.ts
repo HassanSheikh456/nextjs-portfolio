bcd0c0ec3fafeeb0399483d7f302c38960a195c7
Hassan Sheikh <hassan.nadeem.4012@gmail.com>
2026-01-16 20:20:12 +0500

    add NavLinks constant for navigation menu

---
+++ constant/constant.ts
@@ -0,0 +1,37 @@
+export const NavLinks = [
+  {
+    id: 1,
+    lable: 'Home',
+    url: '#'
+  },
+  {
+    id: 2,
+    lable: 'Services',
+    url: '#'
+  },
+  {
+    id: 3,
+    lable: 'Resumes',
+    url: '#'
+  },
+  {
+    id: 4,
+    lable: 'Works',
+    url: '#'
+  },
+  {
+    id: 5,
+    lable: 'Skills',
+    url: '#'
+  },
+  {
+    id: 6,
+    lable: 'Testimonials',
+    url: '#'
+  },
+  {
+    id: 7,
+    lable: 'Contact',
+    url: '#'
+  },
+]
