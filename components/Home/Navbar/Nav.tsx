ee0f5445acabfc0ef509db73a3b03ee302bed56e
Hassan Sheikh <hassan.nadeem.4012@gmail.com>
2026-01-17 15:46:50 +0500

    refactor Nav component to enhance scroll behavior and improve button layout

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,11 +1,27 @@
+"use client";
 import { NavLinks } from "@/constant/constant";
-import { link } from "fs";
-import React from "react";
+import link from "next/link";
+import React, { use, useEffect, useState } from "react";
+import { BiDownload } from "react-icons/bi";
 import { FaCode } from "react-icons/fa";
+import { HiBars3BottomRight } from "react-icons/hi2";
 
 const Nav = () => {
+  const [navBg, setNavBg] = useState(false);
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY > 90) setNavBg(true);
+
+      if (window.scrollY < 90) setNavBg(false);
+    };
+    window.addEventListener("scroll", handler);
+    return () => window.removeEventListener("scroll", handler);
+  }, []);
+
   return (
-    <div className="transition-all duration-200 h-[12vh] z-[10000] fixed w-full">
+    <div
+      className={`transition-all ${navBg ? "bg-[#0f142ed9] shadow-md" : "fixed"} duration-200 h-[12vh] z-10000 fixed w-full`}
+    >
       <div className="flex items-center h-full justify-between w-[90%] mx-auto">
         {/* {LOGO } */}
         <div className="flex items-center space-x-2">
@@ -16,6 +32,7 @@ const Nav = () => {
             Hassan
           </h1>
         </div>
+        {/* NavLinks */}
         <div>
           <div className="hidden lg:flex items-center space-x-10">
             {NavLinks.map((link) => (
@@ -28,6 +45,16 @@ const Nav = () => {
               </a>
             ))}
           </div>
+        </div>
+        {/* Button */}
+        <div className="flex items-center space-x-4">
+          {/*CV Button  */}
+          <button className="px-8 py-3.5 text-sm cursor-pointer rounded-lg bg-blue-800 text-white hover:bg-blue-900 transition-all duration-300 flex items-center space-x-2">
+            <BiDownload className="w-5 h-5" />
+            <span>Download CV</span>
+          </button>
+          {/* Burger Menu */}
+          <HiBars3BottomRight className="text-white w-8 h-8 cursor-pointer lg:hidden" />
         </div>
       </div>
     </div>
