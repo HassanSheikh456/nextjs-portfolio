5063ac480023cacf360b15ad9b3466e4d2bf6574
Hassan Sheikh <hassan.nadeem.4012@gmail.com>
2026-01-20 13:54:41 +0500

    add Hero and ParticleHero components for enhanced visual presentation

---
+++ components/Home/Hero/Hero.tsx
@@ -0,0 +1,45 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import Typewriter from "typewriter-effect";
+import ParticleHero from "./ParticleBackground";
+
+const Hero = () => {
+  return (
+    <div className="relative h-screen flex text-white flex-col justify-center items-center overflow-hidden">
+      <ParticleHero />
+      <div className="relative z-10 flex flex-col items-center">
+        <Image
+          src="/images/asas.png"
+          alt="Hero Image"
+          width={150}
+          height={150}
+          className="rounded-full border-8 border-[#0c0c48aa]"
+        />
+        <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl mt-6 text-center font-bold tracking-wide">
+          Creating web products, <br /> brands,
+                strings: [
+                  " Front end Developer",
+                  " Back end Developer",
+                  " Web Developer",
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 75,
+                deleteSpeed: 50,
+                wrapperClassName: "pl-2",
+              }}
+            />
+          </span>
+        </h2>
+        <button className="mt-6 px-10 py-4 hover:bg-blue-900 bg-blue-800 transition-all duration-500 cursor-pointer rounded-full ">
+          <span >See my work</span>
+          <BsArrowRight className="w-5 h-5 ml-2 inline-block" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
