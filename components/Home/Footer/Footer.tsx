0b291720398bfedb7f8b8dded39c2cf11c54633b
HassanSheikh456 <hassan.nadeem.4012@gmail.com>
2026-01-27 17:25:49 +0500

    add Footer component for website branding and information display

---
+++ components/Home/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-blue-900/20 p-5'>
+        <h1 className='text-center text-white/70 font-bold text-lg'>Hassan - Persional Portfolio Website</h1>
+    </div>
+  )
+}
+
+export default Footer
