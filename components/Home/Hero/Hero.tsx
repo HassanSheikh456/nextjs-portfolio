e82f5b7229595a7abbd654a09a2f0ae5de8d0abc
HassanSheikh456 <hassan.nadeem.4012@gmail.com>
2026-01-27 18:51:35 +0500

    add data-aos attributes for enhanced animation effects in Hero component

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -16,9 +16,17 @@ const Hero = () => {
           width={150}
           height={150}
           className="rounded-full border-8 border-[#0c0c48aa]"
+          data-aos="fade-up"
         />
-        <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl mt-6 text-center font-bold tracking-wide">
+        <h1 data-aos="fade-up" data-aos-delay="200" className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl mt-6 text-center font-bold tracking-wide">
           Creating web products, <br /> brands,
+          <span className="text-cyan-200"> and experiences.</span>
+        </h1>
+        <h2  data-aos="fade-up" data-aos-delay="400" className="text-sm mt-5 px-2 text-center sm:text-2xl font-medium flex items-center">
+          Hi ! I&apos;m Hassan - A Passionate
+          <span className="text-cyan-200 font-bold">
+            <Typewriter
+              options={{
                 strings: [
                   " Front end Developer",
                   " Back end Developer",
@@ -33,7 +41,7 @@ const Hero = () => {
             />
           </span>
         </h2>
-        <button className="mt-6 px-10 py-4 hover:bg-blue-900 bg-blue-800 transition-all duration-500 cursor-pointer rounded-full ">
+        <button  data-aos="fade-up" data-aos-delay="600" className="mt-6 px-10 py-4 hover:bg-blue-900 bg-blue-800 transition-all duration-500 cursor-pointer rounded-full ">
           <span >See my work</span>
           <BsArrowRight className="w-5 h-5 ml-2 inline-block" />
         </button>
