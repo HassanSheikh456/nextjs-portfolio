9a32ed3710403491f2ffe293e3100d9909efcb7c
HassanSheikh456 <hassan.nadeem.4012@gmail.com>
2026-01-27 19:06:56 +0500

    add data-aos-anchor-placement attributes for improved animation control in Services component

--- components/Home/Services/Services.tsx
+++ components/Home/Services/Services.tsx
@@ -9,28 +9,28 @@ const Services = () => {
         result
       </h1>
       <div className="w-[90%] sm:w-[70%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mt-12 items-center mx-auto">
-        <div data-aos="fade-right" >
+        <div data-aos="fade-right" data-aos-anchor-placement="top-center"   >
           <ServicesCard
             icon="/images/s1.png"
             name="UI & UX"
             description="Designing beautiful and functional user interfaces, effictive and enjoyable to use."
           />
         </div>
-        <div data-aos="fade-right" data-aos-delay="200">
+        <div data-aos="fade-right" data-aos-delay="200" data-aos-anchor-placement="top-center">
           <ServicesCard
             icon="/images/s2.png"
             name="Web and Mobile App"
             description="Designing beautiful and functional user interfaces, effictive and enjoyable to use."
           />
         </div>
-        <div data-aos="fade-right" data-aos-delay="400">
+        <div data-aos="fade-right" data-aos-delay="400" data-aos-anchor-placement="top-center">
           <ServicesCard
             icon="/images/s3.png"
             name="Design & Creative"
             description="Designing beautiful and functional user interfaces, effictive and enjoyable to use."
           />
         </div>
-        <div data-aos="fade-right" data-aos-delay="600">
+        <div data-aos="fade-right" data-aos-delay="600" data-aos-anchor-placement="top-center">
           <ServicesCard
             icon="/images/s4.png"
             name="Development"
