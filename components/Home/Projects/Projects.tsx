3aea117cad8ffe96c231cc455e055f7a8decfe75
HassanSheikh456 <hassan.nadeem.4012@gmail.com>
2026-01-27 19:21:09 +0500

    add data-aos attributes for enhanced animation effects in Projects component

--- components/Home/Projects/Projects.tsx
+++ components/Home/Projects/Projects.tsx
@@ -10,52 +10,48 @@ const Projects = () => {
       </h1>
       <div className="w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 mt-16">
         {/* 1st Project */}
-        <div>
-          <Image
-            src="/images/p1.jpg"
-            alt="Img"
-            width={800}
-            height={650}
-          />
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="0"
+        >
+          <Image src="/images/p1.jpg" alt="Img" width={800} height={650} />
           <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">
             Modern Finance Dashboard UI
           </h1>
           <h1 className="pt-2 font-medium text-white/80 ">Apps , UI/UX</h1>
         </div>
         {/* 2nd Project */}
-        <div>
-          <Image
-            src="/images/p2.jpg"
-            alt="Img"
-            width={800}
-            height={650}
-          />
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="100"
+        >
+          <Image src="/images/p2.jpg" alt="Img" width={800} height={650} />
           <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">
             Portfolio Website for creatives
           </h1>
           <h1 className="pt-2 font-medium text-white/80 ">Branding , Motion</h1>
         </div>
         {/* 3rd Project */}
-        <div>
-          <Image
-            src="/images/p3.jpg"
-            alt="Img"
-            width={800}
-            height={650}
-          />
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="200"
+        >
+          <Image src="/images/p3.jpg" alt="Img" width={800} height={650} />
           <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">
             Branding Identity for startup
           </h1>
           <h1 className="pt-2 font-medium text-white/80 ">Branding , UI/UX</h1>
         </div>
         {/* 4st Project */}
-        <div>
-          <Image
-            src="/images/p4.jpg"
-            alt="Img"
-            width={800}
-            height={650}
-          />
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="300"
+        >
+          <Image src="/images/p4.jpg" alt="Img" width={800} height={650} />
           <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">
             SaaS Dashboard Development
           </h1>
